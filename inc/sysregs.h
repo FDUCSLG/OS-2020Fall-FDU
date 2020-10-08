e36c6b8019b5ef16da5d0eafb7d42aa58345e7fd
lhq <lihongqinzhuanyong@gmail.com>
2020-10-09 07:00:44 +0800

    Refactor entry.S

--- inc/sysregs.h
+++ inc/sysregs.h
@@ -1,35 +1,40 @@
 #ifndef INC_SYSREGS_H
 #define INC_SYSREGS_H
 
-/* SCTLR_EL1, System Control Register (EL1) */
-#define SCTLR_RESERVED                  (3 << 28) | (3 << 22) | (1 << 20) | (1 << 11)
-#define SCTLR_EE_LITTLE_ENDIAN          (0 << 25)
-#define SCTLR_EOE_LITTLE_ENDIAN         (0 << 24)
-#define SCTLR_I_CACHE_DISABLED          (0 << 12)
-#define SCTLR_D_CACHE_DISABLED          (0 << 2)
-#define SCTLR_MMU_DISABLED              (0 << 0)
-#define SCTLR_MMU_ENABLED               (1 << 0)
+/* SCTLR_EL1, System Control Register (EL1). */
+#define SCTLR_RESERVED              ((3 << 28) | (3 << 22) | (1 << 20) | (1 << 11) | (1 << 8) | (1 << 7))
+#define SCTLR_EE_LITTLE_ENDIAN      (0 << 25)
+#define SCTLR_E0E_LITTLE_ENDIAN     (0 << 24)
+#define SCTLR_I_CACHE               (1 << 12)
+#define SCTLR_D_CACHE               (1 << 2)
+#define SCTLR_MMU_DISABLED          (0 << 0)
+#define SCTLR_MMU_ENABLED           (1 << 0)
 
-#define SCTLR_VALUE_MMU_DISABLED    (SCTLR_RESERVED | SCTLR_EE_LITTLE_ENDIAN | SCTLR_I_CACHE_DISABLED | SCTLR_D_CACHE_DISABLED | SCTLR_MMU_DISABLED)
+#define SCTLR_VALUE_MMU_DISABLED    (SCTLR_RESERVED | SCTLR_EE_LITTLE_ENDIAN | SCTLR_E0E_LITTLE_ENDIAN | SCTLR_I_CACHE | SCTLR_D_CACHE | SCTLR_MMU_DISABLED)
 
-/* HCR_EL2, Hypervisor Configuration Register (EL2) */
-#define HCR_RW               (1 << 31)
-#define HCR_VALUE            HCR_RW
+/* HCR_EL2, Hypervisor Configuration Register (EL2). */
+#define HCR_RW                      (1 << 31)
+#define HCR_VALUE                   HCR_RW
 
-/* SCR_EL3, Secure Configuration Register (EL3) */
-#define SCR_RESERVED          (3 << 4)
-#define SCR_RW                (1 << 10)
-#define SCR_NS                (1 << 0)
-#define SCR_VALUE             (SCR_RESERVED | SCR_RW | SCR_NS)
+/* SCR_EL3, Secure Configuration Register (EL3). */
+#define SCR_RESERVED                (3 << 4)
+#define SCR_RW                      (1 << 10)
+#define SCR_HCE                     (1 << 8)
+#define SCR_SMD                     (1 << 7)
+#define SCR_NS                      (1 << 0)
+#define SCR_VALUE                   (SCR_RESERVED | SCR_RW | SCR_HCE | SCR_SMD | SCR_NS)
 
-/* SPSR_EL3, Saved Program Status Register (EL3) */
-#define SPSR_MASK_ALL        (7 << 6)
-#define SPSR_EL1h            (5 << 0)
-#define SPSR_VALUE           (SPSR_MASK_ALL | SPSR_EL1h)
+/* SPSR_EL1/2/3, Saved Program Status Register. */
+#define SPSR_MASK_ALL               (7 << 6)
+#define SPSR_EL1h                   (5 << 0)
+#define SPSR_EL2h                   (9 << 0)
+#define SPSR_EL3_VALUE              (SPSR_MASK_ALL | SPSR_EL2h)
+#define SPSR_EL2_VALUE              (SPSR_MASK_ALL | SPSR_EL1h)
 
-/* ESR_EL1, Exception Syndrome Register (EL1) */
-#define ESR_ELx_EC_SHIFT        26
-#define ESR_ELx_EC_SVC64        0x15
-#define ESR_ELx_EC_DABT_LOW     0x24
+/* Exception Class in ESR_EL1. */
+#define EC_SHIFT                    26
+#define EC_SVC64                    0x15
+#define EC_DABORT                   0x24
+#define EC_IABORT                   0x20
 
 #endif
